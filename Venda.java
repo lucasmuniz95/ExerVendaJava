32297ed449b316c32ea11bda609d8543a4b08ccd
lucasmuniz95 <100134869+lucasmuniz95@users.noreply.github.com>
2022-10-15 23:29:53 -0300

    Update Venda.java

--- Venda.java
+++ Venda.java
@@ -31,34 +31,44 @@ public class Venda {
         this.quantidadeItens = quantidadeItens;
     }
 
-    public calcularValor(int quantidadeItens, Produto.valorVenda, int desconto){
-        valor = quantidadeItens * efetuarDesconto(int desconto);
+    public float calcularValor(){
+        float valor = quantidadeItens * produto.getValorVenda();
         return valor;
     }
 
-    public efetuarDesconto(int desconto){
-        descontoReal = Produto.valorVenda - (Produto.valorVenda - desconto)
-        return descontoReal;
-    }
-
-    public calcularComissao(Vendedor.comissao, Produto.valorVenda){
-        if (Produto.promocao == true){
-            comissaoReal = Produto.valorVenda * (Vendedor.comissao/2)
-        } else {
-            comissaoReal = Produto.valorVenda * comissao
+    public float efetuarDesconto(){
+        float valorDesconto = produto.getValorVenda()*100;
+        float valorProdutoDesconto = produto.getValorVenda()-valorDesconto;
+        if(valorProdutoDesconto<produto.getValorCusto()) {
+        	System.out.printf("Cupom de desconto não se aplica");
+        	return produto.getValorVenda();
         }
-        return comissaoReal;
+        return valorProdutoDesconto;
+        
     }
 
-    public Imprimir(){
-        System.out.println('Codigo do vendedor: ' + Vendedor.codigo + " - Nome: " + Vendedor.nome + " - Comissão: " + Vendedor.comissao);
-        System.out.println("Quantidade de itens vendidos: " + QuantidadeItens);
-        System.out.println("Codigo do Produto: " + Produto.codigo + " - Descrisção do produto: " + Produto.descriscao);
-        System.out.println("Valor de venda do produto: " + (Produto.valorVenda / quantidadeItens);
-        System.out.println("Produto está em promoção: " + Produto.promocao);
-        System.out.println("Valor do desconto sobre o produto: " + efetuarDesconto(desconto));
-        System.out.println("Valor total da venda: " + Produto.valorVenda);
+    public float calcularComissao(){
+    	float valorComissao = (float)(valor*vendedor.getComissao());
+        if (produto.getPromocao()){
+            valorComissao = valorComissao/2;
+        } 
+        return valorComissao;
     }
+    
+
+    public  void Imprimir(){
+        System.out.println("Codigo do vendedor: " + vendedor.codigo + " - Nome: " + vendedor.nome + " - Comissão: " + vendedor.comissao);
+        System.out.println("Quantidade de itens vendidos: " + quantidadeItens);
+        System.out.println("Codigo do Produto: " + produto.codigo + " - Descrisção do produto: " + produto.descriscao);
+        System.out.println("Valor de venda do produto: " + produto.valorVenda / quantidadeItens);
+        System.out.println("Produto está em promoção: " + produto.promocao);
+        System.out.println("Valor do desconto sobre o produto: " + efetuarDesconto(desconto));
+        System.out.println("Valor total da venda: " + produto.valorVenda);
+    }
+
+	private String efetuarDesconto(int desconto2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
-
