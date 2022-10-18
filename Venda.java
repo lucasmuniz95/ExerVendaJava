0c0c6027c6fbc33741e3723b738a5b760ac3393b
lucasmuniz95 <100134869+lucasmuniz95@users.noreply.github.com>
2022-10-18 17:40:58 -0300

    Update Venda.java

--- Venda.java
+++ Venda.java
@@ -14,39 +14,39 @@ public class Venda {
         return produto;
     }
 
-    public void setProduto(Produto produto) {
+    public static void setProduto(Produto produto) {
         this.produto = produto;
     }
 
-    public Vendedor getVendedor() {
+    public static Vendedor getVendedor() {
         return vendedor;
     }
 
-    public void setVendedor(Vendedor vendedor) {
+    public static void setVendedor(Vendedor vendedor) {
         this.vendedor = vendedor;
     }
 
-    public int getQuantidadeItens() {
+    public static int getQuantidadeItens() {
         return quantidadeItens;
     }
 
-    public void setQuantidadeItens(int quantidadeItens) {
+    public static void setQuantidadeItens(int quantidadeItens) {
         this.quantidadeItens = quantidadeItens;
     }
 
-    public void descontoParaPorcetagem(float desconto){
+    public static void descontoParaPorcetagem(float desconto){
         float descontoP = (desconto * 100) / valor;
     }
 
-    public void calcularValor(){
-       float valor = (quantidadeItens * getProduto().getValorVenda()) - desconto;
+    public static void calcularValor(){
+       float valor = (quantidadeItens * Produto.getValorVenda()) - desconto;
     }
 
 
     public void efetuarDesconto(float desconto){
         float valorReal = valor - desconto;
         float valoresProdutos = valorReal / quantidadeItens;
-        if (valoresProdutos < getProduto().getValorCusto()){
+        if (valoresProdutos < Produto.getValorCusto()){
             System.out.println("Desconto acima do permitido.");;
         } else {
             System.out.println("Desconto permitido.");
@@ -54,21 +54,21 @@ public class Venda {
     }
 
     public void calcularComissao(float Vendedor.comissao){
-        if (getProduto().isPromocao()){
-            float comissaoReal = getProduto().getValorVenda() * (Vendedor.comissao/2);
+        if (Produto.isPromocao()){
+            float comissaoReal = Produto.getValorVenda() * (Vendedor.comissao/2);
         } else{
-            float comissaoReal = getProduto().getValorVenda() * Vendedor.comissao;
+            float comissaoReal = Produto.getValorVenda() * Vendedor.comissao;
         }
     }
 
-    public  void Imprimir(){
-        System.out.println("Codigo do vendedor: " + getVendedor().getCodigo() + " - Nome: " + getVendedor().getNome() + " - Comissão: " + Vendedor.comissao);
+    public void Imprimir(){
+        System.out.println("Codigo do vendedor: " + Vendedor.getCodigo() + " - Nome: " + Vendedor.getNome() + " - Comissão: " + Vendedor.comissao);
         System.out.println("Quantidade de itens vendidos: " + quantidadeItens);
-        System.out.println("Codigo do Produto: " + getProduto().getCodigoProduto() + " - Descrisção do produto: " + getProduto().getDescricao());
-        System.out.println("Valor de venda do produto: " + getProduto().getValorVenda() / quantidadeItens);
-        System.out.println("Produto está em promoção: " + getProduto().isPromocao());
-        System.out.println("Valor do desconto sobre o produto: " + efetuarDesconto(desconto));
-        System.out.println("Valor total da venda: " + getProduto().getValorVenda());
+        System.out.println("Codigo do Produto: " + Produto.getCodigoProduto() + " - Descrisção do produto: " + Produto.getDescricao());
+        System.out.println("Valor de venda do produto: " + Produto.getValorVenda() / quantidadeItens);
+        System.out.println("Produto está em promoção: " + Produto.isPromocao());
+        System.out.println("Valor do desconto sobre o produto: " + (efetuarDesconto(desconto);
+        System.out.println("Valor total da venda: " + Produto.getValorVenda());
     }
 
 }
